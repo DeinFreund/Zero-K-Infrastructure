815ac06aea94915e8545c8001e6a3d1991eda004
DeinFreund <deinbestrfreund@gmail.com>
2017-12-11 01:48:40 +0100

    ignore username case on login

--- ZkLobbyServer/LoginChecker.cs
+++ ZkLobbyServer/LoginChecker.cs
@@ -68,7 +68,7 @@ namespace ZkLobbyServer
                     Account accBySteamID = null;
                     Account accByLogin = null;
                     if (info != null) accBySteamID = db.Accounts.Include(x => x.Clan).Include(x => x.Faction).FirstOrDefault(x => x.SteamID == info.steamid);
-                    if (!string.IsNullOrEmpty(login.Name)) accByLogin = db.Accounts.Include(x => x.Clan).Include(x => x.Faction).FirstOrDefault(x => x.Name == login.Name);
+                    if (!string.IsNullOrEmpty(login.Name)) accByLogin = db.Accounts.Include(x => x.Clan).Include(x => x.Faction).FirstOrDefault(x => x.Name == login.Name) ?? db.Accounts.Include(x => x.Clan).Include(x => x.Faction).FirstOrDefault(x => x.Name.Equals(login.Name, StringComparison.OrdinalIgnoreCase));
 
                     if (accBySteamID == null)
                     {
